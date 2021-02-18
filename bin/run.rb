107bf2a70a35a9fbf987e9e450d1f4131bea8b36
Taylor Treece <taylor.treece@protonmail.com>
2021-02-18 13:41:09 -0600

    commit tree f9d3463a0ad5913af354fe233eb06d3a2d1c5909

--- bin/run.rb
+++ bin/run.rb
@@ -2,6 +2,8 @@ require_relative "environment"
 
 Scraper.new(@db).scrape
 
+DB = {:conn => SQLite3::Database.new("db/pokemon.db")}
+
 all_pokemon = @db.execute("SELECT * FROM pokemon;")
 
 # test out your code here!
